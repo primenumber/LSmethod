d8fafac0dd1a3af1041b02011800781de35b259d
primenumber <prime@kmc.gr.jp>
2016-08-16 08:42:16 +0900

    use MKL_INT

--- matrix.hpp
+++ matrix.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include "level0.hpp"
 #include "level1.hpp"
+#include "mkl.h"
 
 namespace math {
 
 struct SpMat {
-  std::vector<int> cols;
+  std::vector<MKL_INT> cols;
   Vector elems;
-  std::vector<int> row_offsets;
+  std::vector<MKL_INT> row_offsets;
   int col_size;
   int row_size;
 };
