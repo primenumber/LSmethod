303b8f75ad299c52fd83d9e56f905efb5a68bbab
primenumber <prime@kmc.gr.jp>
2016-08-16 08:41:25 +0900

    fix trans_mv

--- level2.cpp
+++ level2.cpp
@@ -15,10 +15,11 @@ Vector operator*(const SpMat &lhs, const Vector &rhs) {
 }
 
 Vector trans_mv(const SpMat &lhs, const Vector &rhs) {
-  Vector res(lhs.row_size);
+  Vector res(std::max(lhs.col_size, lhs.row_size));
   char n = 'T';
   mkl_cspblas_dcsrgemv(&n, &lhs.row_size, lhs.elems.data(), lhs.row_offsets.data(),
       lhs.cols.data(), rhs.data(), res.data());
+  res.resize(lhs.col_size);
   return res;
 }
 
